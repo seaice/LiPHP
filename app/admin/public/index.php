d09bbeef063fef3a8e3f0616c8401f5ab5dd147d
seaice <haibing1458@163.com>
2017-11-29 15:33:05 +0800

    优化

--- app/admin/public/index.php
+++ app/admin/public/index.php
@@ -1,9 +1,6 @@
 <?php
 namespace admin;
 
-// error_reporting(E_ALL ^ E_NOTICE);
-// error_reporting(0);
-
 define('PATH_APP', realpath(__DIR__).'\\'.'..'.'\\');
 define('ENV', 'dev');       // 配置文件目录
 
--- app/gii/public/index.php
+++ app/gii/public/index.php
@@ -1,10 +1,8 @@
 <?php
 namespace gii;
 
-error_reporting(E_ALL ^ E_NOTICE);
 define('PATH_APP', realpath(__DIR__).'\\'.'..'.'\\');
 define('ENV', 'dev');       // 配置文件目录
-define('DEBUG', true);      // 开发或者调试模式
 
 include('../../../vendor/autoload.php');
 \Li\App::init()->run(__NAMESPACE__);
